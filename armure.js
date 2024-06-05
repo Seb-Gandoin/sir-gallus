4dc968b4005193338495790bfbd21c3257ed24aa
Sébastien GANDOIN <sebastien_gandoin@yahoo.fr>
2024-06-05 11:10:17 +0200

    add music

--- armure.js
+++ armure.js
@@ -1,3 +1,21 @@
+let audio = document.getElementById("bg-music");
+let playPauseButton = document.getElementById("play-pause-button");
+let closeBtn = document.getElementsByClassName("close")[0];
+
+
+playPauseButton.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.textContent = "pause";
+    } else {
+        audio.pause();
+        playPauseButton.textContent = "play";
+    }
+});
+
+
+
+
 let popup = document.querySelector('#popup-overlay');
 let popupSound = new Audio();
     popupSound.src = "assets/Popup.mp3";
