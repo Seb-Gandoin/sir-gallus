defe32ee522cb8760ed1cfcc7881ed7b84ba61a9
Fei0323 <fei.bao.guo@gmail.com>
2024-06-05 22:29:10 +0200

    updete style

--- enigme.js
+++ enigme.js
@@ -1,9 +1,5 @@
 let audio = document.getElementById("bg-music");
 let playPauseButton = document.getElementById("play-pause-button");
-let answerInput = document.getElementById("answer-input");
-let closeBtn = document.getElementsByClassName("close")[0];
-
-
 playPauseButton.addEventListener("click", function () {
     if (audio.paused) {
         audio.play();
@@ -14,9 +10,17 @@ playPauseButton.addEventListener("click", function () {
     }
 });
 
+function redirectToPage() {
+    window.location.href = "03bis.html";
+}
+
+let answerInput = document.getElementById("answer-input");
+/*
+let closeBtn = document.getElementsByClassName("close")[0];
+*/
 let popup = document.querySelector('#popup-overlay');
 let popupSound = new Audio();
-popupSound.src = "assets/Popup.mp3";
+popupSound.src = "assets/assets_Popup.mp3";
 answerInput.addEventListener("change", function () {
     console.log("poulet")
     if (answerInput.value.toLowerCase() === "ombre") {
