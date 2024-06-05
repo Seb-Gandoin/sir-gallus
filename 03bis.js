2f49cac72e8e9f0f755f42bed16fbe88f354aca4
Fei0323 <fei.bao.guo@gmail.com>
2024-06-05 10:29:56 +0200

    add all files

---
+++ 03bis.js
@@ -0,0 +1,12 @@
+let audio = document.getElementById("bg-music");
+let playPauseButton = document.getElementById("play-pause-button");
+
+playPauseButton.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play();
+        playPauseButton.textContent = "pause";
+    } else {
+        audio.pause();
+        playPauseButton.textContent = "play";
+    }
+});
