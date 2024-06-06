2124dcc16309f0959df0897bc4ede99fa8354bfd
Fei0323 <fei.bao.guo@gmail.com>
2024-06-06 12:08:12 +0200

    finish round 1

--- 03bis.js
+++ 03bis.js
@@ -1,32 +1,22 @@
 let audio = document.getElementById("bg-music");
 let playPauseButton = document.getElementById("play-pause-button");
-
 playPauseButton.addEventListener("click", function () {
     if (audio.paused) {
         audio.play();
-        playPauseButton.textContent = "pause";
+        playPauseButton.textContent = "⏸";
     } else {
         audio.pause();
-        playPauseButton.textContent = "play";
+        playPauseButton.textContent = "♬";
     }
 });
+
+function redirectToHome() {
+    window.location.href = "accueil.html";
+}
 
 function redirectToPage() {
     window.location.href = "armure.html";
 }
-
-
-/*
-document.getElementById('zoomButton').addEventListener('click', function (event) {
-    event.preventDefault();
-    this.innerText = "C'est parti";
-    this.classList.add('zoomed');
-    setTimeout(() => {
-        window.location.href = document.getElementById('zoomLink').href;
-    }, 500);
-});
-*/
-
 
 const buttons = document.querySelectorAll('.zoomButton');
 
